f93e05cf0887b4e4d05a2ef84ff2efd9ca1033aa
Hansith <hansithlochana@gmail.com>
2024-08-18 06:22:28 +0530

    Set components
    setting the components

--- contact-app/src/components/App.js
+++ contact-app/src/components/App.js
@@ -1,8 +1,31 @@
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+import AddContact from "./AddContact";
+import ContactList from "./ContactList";
+
 import './App.css';
 
 function App() {
+  const contacts = [
+    {
+      id: "1",
+      name: "Jimmy",
+      email: "jimmyandi@gmail.com",
+    },
+    {
+      id: "2",
+      name: "Broad",
+      email: "broadst@gmail.com"
+    },
+  ];
+
   return (
-    <div>Hello world</div>
+    <div className="ui container">
+      <Header />
+      <AddContact/>
+      <ContactList contacts = {contacts}/>
+    </div>
   );
 }
 
